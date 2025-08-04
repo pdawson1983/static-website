4b0fc7d74703e05eb27b035e70311c9ea6301f94
Paul Dawson <pdawson1983@gmail.com>
2025-08-04 00:25:37 -0400

    added text_to_textnodes to convert text into textnodes

--- src/test_mdhandler.py
+++ src/test_mdhandler.py
@@ -1,6 +1,8 @@
 import unittest
 
-from mdhandler import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from mdhandler import split_nodes_delimiter, \
+    extract_markdown_images, extract_markdown_links, \
+        split_nodes_image, split_nodes_link, text_to_textnodes
 from textnode import TextNode, TextType
 
 class TestMdHandler(unittest.TestCase):
@@ -132,6 +134,29 @@ class TestMdHandler(unittest.TestCase):
                     "second link",
                     TextType.LINK,
                     "https://bing.com")
+            ]
+        )
+    
+    def test_text_to_textnodes(self):
+        text = (
+            "This is **text** with an _italic_ word and a `code block`"
+            " and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg)" 
+            " and a [link](https://boot.dev)"
+        )
+        nodes = text_to_textnodes(text)
+        self.assertEqual(
+            nodes,
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
             ]
         )
 
