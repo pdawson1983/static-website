bc2f7fbc4024afb10fe19306759d0a68a15fcaf1
Paul Dawson <pdawson1983@gmail.com>
2025-08-05 00:47:18 -0400

    update gitignore and add todos in main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-from textnode import TextNode, TextType
+from os import path, listdir, mkdir
+from shutil import copy, rmtree
+
+def copy_all_files_to_destination(source,destination):
+    pass #TODO Add recursive function to copies all the contents from a source directory to a destination directory (in our case, static to public)
 
 def main():
-    testNode = TextNode('This is some anchor text', 'link', 'https://www.boot.dev')
-    print(testNode)
+    pass #TODO Hook in copy_all_files_to_destination 
 
 if __name__ == '__main__':
     main()
